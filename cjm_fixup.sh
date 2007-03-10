01f89356b8550135109b2a930690fc82e4caec13
Christopher J. Madsen <perl@cjmweb.net>
2007-03-10 17:10:01 -0600

    Prepare to release version 1.09
      Update Changes, MANIFEST, MANIFEST.SKIP, README
      Change Makefile.PL to use VERSION_FROM
      cjm_fixup uses 2-digit version number

--- cjm_fixup.sh
+++ cjm_fixup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 chmod a+w -R $1
-perl /util/bin/vernum.pl -pd3 $1 lib/Getopt/Mixed.pm README
+perl /util/bin/vernum.pl -rpd2 $1 lib/Getopt/Mixed.pm README
 gnufind $1 -type f -exec flip -uv \{\} \;
 chmod a-w -R $1
