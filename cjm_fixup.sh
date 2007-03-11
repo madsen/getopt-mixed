7f973376164977833bc86190a06de71c0fa6e282
Christopher J. Madsen <perl@cjmweb.net>
2007-03-10 19:13:03 -0600

    Fix Makefile.PL & cjm_fixup.sh to build on Unix
      Only call cjm_fixup if it exists
      Rename cjm_fixup.btm to cjm_fixup.bat

--- cjm_fixup.sh
+++ cjm_fixup.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-chmod a+w -R $1
-perl /util/bin/vernum.pl -rpd2 $1 lib/Getopt/Mixed.pm README
-gnufind $1 -type f -exec flip -uv \{\} \;
-chmod a-w -R $1
+vernum -rpd2 $1 lib/Getopt/Mixed.pm README
